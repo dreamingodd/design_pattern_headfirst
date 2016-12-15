e1f6fd429f5beeb412b4fa763043a805dea6256b
dreamingodd <memorywilllast@gmail.com>
2016-12-15 19:29:05 +0800

    Composite Test adjustments.

--- src/composite/test/Test.java
+++ src/composite/test/Test.java
@@ -1,28 +1,27 @@
 package composite.test;
 
-
 import composite.component.MenuComponent;
 import composite.entity.Menu;
 import composite.entity.MenuItem;
 
 public class Test {
-	public static void main(String[] args) {
-		MenuComponent pancakeMenu = new Menu("Pancake Menu");
-		MenuComponent cafeMenu = new Menu("Cafe Menu");
-		MenuComponent dessertMenu = new Menu("Desset Menu");
+    public static void main(String[] args) {
+        MenuComponent pancakeMenu = new Menu("Pancake Menu");
+        MenuComponent cafeMenu = new Menu("Cafe Menu");
+        MenuComponent dessertMenu = new Menu("Desset Menu");
 
-		pancakeMenu.add(new MenuItem("pancake1"));
-		cafeMenu.add(new MenuItem("cafe1"));
-		dessertMenu.add(new MenuItem("dessert1"));
+        pancakeMenu.add(new MenuItem("pancake1"));
+        cafeMenu.add(new MenuItem("cafe1"));
+        dessertMenu.add(new MenuItem("dessert1"));
 
-		/*************************************/
-		MenuComponent allMenu = new Menu("All");
-		/*************************************/
+        /*************************************/
+        MenuComponent allMenu = new Menu("All");
+        /*************************************/
 
-		allMenu.add(pancakeMenu);
-		allMenu.add(cafeMenu);
-		allMenu.add(dessertMenu);
+        allMenu.add(pancakeMenu);
+        allMenu.add(cafeMenu);
+        allMenu.add(dessertMenu);
 
-		System.out.println(allMenu);
-	}
+        System.out.println(allMenu);
+    }
 }
